caba0d658c7761ad638fe9334ec54b645cbda167
Scot MacDonald <scotmacdonald@hotmail.com>
2023-11-01 08:03:49 +0100

    styles

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,8 @@
 import ListItem from "./ListItem";
+import "./List.css";
 export const List = ({ activities, onDeleteActivity }) => {
   return (
-    <ul className="list__item-ul">
+    <ul className="list">
       {activities.map(({ id, name }) => {
         return (
           <ListItem key={id}>
